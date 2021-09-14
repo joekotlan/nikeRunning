332a3ba3513d62f6a00f11e81d36daed4731f24b
Joe Kotlan <jkotlan@crescendocollective.com>
2021-09-13 21:38:15 -0500

    Initial Commit

---
+++ nikeRunning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nikeRunning
+//
+//  Created by Joe Kotlan on 9/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nikeRunning/nikeRunningApp.swift
@@ -0,0 +1,17 @@
+//
+//  nikeRunningApp.swift
+//  nikeRunning
+//
+//  Created by Joe Kotlan on 9/13/21.
+//
+
+import SwiftUI
+
+@main
+struct nikeRunningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
